5cbbeb5758ab2f2e6ebff0df95185f3af86b2fd7
Rang <Ranggaadp07@gmail.com>
2026-06-22 23:11:07 +0700

    Menambahkan musuh dan juga tracking musuh

--- world/grass.gd
+++ world/grass.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-const GRASS_EFFECT = preload("res://effects/grass_effect.tscn")
+@export var GRASS_EFFECT: PackedScene
+
 @onready var area_2d: Area2D = $Area2D
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	area_2d.area_entered.connect(_on_area_2d_area_entered)
 
